6a78756b309f512dcca943a57e0a6aaef7c74b22
Tanay PrabhuDesai <tanayseven@gmail.com>
2020-10-18 17:13:57 +0100

    Initial commit

---
+++ src/test/kotlin/com/tanayseven/digitalwallet/DigitalWalletApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tanayseven.digitalwallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DigitalWalletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
